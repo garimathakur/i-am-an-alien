aaf6dee00ff7a868d11847097aec43da842e52c5
Thomas Burke <etburke4@gmail.com>
2017-11-01 21:40:48 -0700

    more blue text

--- src/scenes/e/2.js
+++ src/scenes/e/2.js
@@ -3,8 +3,8 @@ import { Block, Text, Link } from '../elements';
 
 const Component = () => (
   <Block>
-    <Text>Borders have guards and the guards have guns</Text>
-    <Text>620 sheets of paper are rustling in the wind</Text>
+    <Text textColor='blue'>Borders have guards and the guards have guns</Text>
+    <Text textColor='blue'>620 sheets of paper are rustling in the wind</Text>
     <Text><Link to='/a/1'>You are running against the wind to collate</Link></Text>
   </Block>
 );
