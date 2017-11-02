90eccb802f7a2eafac911d49d9c63abd1eb880ba
Thomas Burke <etburke4@gmail.com>
2017-11-01 21:42:00 -0700

    slower splendor of race

--- src/scenes/elements/Text.js
+++ src/scenes/elements/Text.js
@@ -7,7 +7,7 @@ export const Text = styled.p`
   color: ${props => colors[props.textColor]};
   transform-origin: left center;
   ${props => props.scale ?
-    `animation: ${scale} 0.7s ease 0s 1 normal forwards`
+    `animation: ${scale} 1s ease 0s 1 normal forwards`
     : ''
   }
 `;
