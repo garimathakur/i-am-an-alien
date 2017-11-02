2be11938738b6c0ccff8fc1070fca264d9556b5c
Thomas Burke <etburke4@gmail.com>
2017-11-01 21:02:23 -0700

    styling tweaks

--- src/scenes/elements/colors.js
+++ src/scenes/elements/colors.js
@@ -1,6 +1,7 @@
 const yellow = 'rgba(247,236,34,1)';
-const pink = 'rgba(244,127,113,1)';
-const red = 'rgba(44,18,14,1)';
+const pink = 'white';
+// const red = 'rgba(44,18,14,1)';
+const red = 'rgba(210,35,42,1)';
 const white = 'white';
 const maroon = 'rgba(91,34,18,1)';
 
